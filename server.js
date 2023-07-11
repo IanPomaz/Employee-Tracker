2efaa8588677e30633b7a1488196a15e0782ed57
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-07-11 17:49:27 -0400

    fixed prompts added add a department

--- server.js
+++ server.js
@@ -1,10 +1,125 @@
-const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const logo = require("asciiart-logo")
 const PORT = process.env.PORT || 3001;
-const app = express();
 require("console.table")
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host:"localhost",
+        user:"root",
+        password:"19741974aA",
+        database:"employeedb"
+    },
+    console.log("connected to database")
+);
+
+function init(){
+    const logotext= logo({name:"employee tracker"}).render()
+    console.log(logotext)
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "openingMessage",
+        message: "What would you like to do?",
+        choices: [
+          "viewAllEmployees",
+          "viewAllDepartments",
+          "viewAllRoles",
+          "addADepartment",
+          "addARole",
+          "addAEmployee",
+          "quit",
+        ],
+    }
+    ]).then((inquirerresponses)=>{
+        let choices = inquirerresponses.openingMessage;
+        switch(choices){
+            case "viewAllEmployees":
+            viewAllEmployees();
+            break;
+          case "viewAllDepartments":
+            viewAllDepartments();
+            break;
+          case "viewAllRoles":
+            viewAllRoles();
+            break;
+          case "addADepartment":
+            addADepartment();
+            break;
+          case "addARole":
+            addARole();
+            break;
+          case "addAEmployee":
+            addAEmployee();
+            break;
+          case "quit":
+            quit();
+            break;
+          default:
+            console.log("somethings wrong with you");
+            break;
+        }
+    })
+}
+
+function viewAllEmployees(){
+    db.query(`
+      SELECT 
+        e.id,
+        e.first_name,
+        e.last_name,
+        r.title,
+        d.department_name department,
+        r.salary,
+        concat(e2.first_name, " ",e2.last_name) manager
+      FROM employee e 
+      JOIN role r
+      ON e.role_id=r.id
+      JOIN department d
+      ON r.department_id=d.id
+      LEFT JOIN employee e2
+      ON e.manager_id=e2.id
+      `, function(err, results){
+        (err) ? console.log(err) : console.table(results), init()
+    })
+}
+function viewAllDepartments(){
+    db.query("SELECT * FROM department", function(err, results){
+        (err) ? console.log(err) : console.table(results), init()
+    })
+}
+function viewAllRoles(){
+    db.query(`
+      SELECT
+        r.id,
+        r.title,
+        r.salary,
+        d.department_name department
+      FROM role r
+      JOIN department d
+      ON r.department_id=d.id
+    `, function(err, results){
+        (err) ? console.log(err) : console.table(results), init()
+    })
+}
+
+async function addADepartment(){
+    inquirer.prompt([
+      {
+        type:"input",
+        name:"department_name",
+        message:"What is the department name?"
+      }
+    ]).then(({department_name})=>{
+      db.query(`INSERT INTO department SET ?`, {department_name}, (err,data)=>{
+          if(err) return console.log(err);
+          init()
+
+        }
+
+      )
+    })
+}
+init()
